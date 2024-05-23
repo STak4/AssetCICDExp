a9b2e815791d34b0f6c4f686fc97914bb4491610
STak4 <takagicontact@gmail.com>
2024-05-24 01:33:18 +0900

    S3から読み込み成功

--- Assets/Scripts/Loader/SampleLoader.cs
+++ Assets/Scripts/Loader/SampleLoader.cs
@@ -7,13 +7,14 @@ namespace STak4.AssetCICD.Loader
     public class SampleLoader : MonoBehaviour
     {
         [SerializeField] private AssetReference _assetReference;
+        [SerializeField] private string _key;
 
         [SerializeField] private RawImage _target;
         
         // Start is called before the first frame update
         async void Start()
         {
-            var tex2D = await Addressables.LoadAssetAsync<Texture2D>(_assetReference).Task;
+            var tex2D = await Addressables.LoadAssetAsync<Texture2D>(_key).Task;
             _target.texture = tex2D;
         }
 
