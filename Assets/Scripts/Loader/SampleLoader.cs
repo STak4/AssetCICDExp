08543493ef361e5ef1077db4fe893ce5fea2ed01
STak4 <takagicontact@gmail.com>
2024-05-24 01:11:20 +0900

    Addressable読み込みテスト

---
+++ Assets/Scripts/Loader/SampleLoader.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+
+namespace STak4.AssetCICD.Loader
+{
+    public class SampleLoader : MonoBehaviour
+    {
+        [SerializeField] private AssetReference _assetReference;
+
+        [SerializeField] private RawImage _target;
+        
+        // Start is called before the first frame update
+        async void Start()
+        {
+            var tex2D = await Addressables.LoadAssetAsync<Texture2D>(_assetReference).Task;
+            _target.texture = tex2D;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
