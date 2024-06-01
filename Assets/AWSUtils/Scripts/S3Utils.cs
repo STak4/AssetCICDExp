f9ead8b90e24fe624e5d22bdca3f884c03371340
STak4 <takagicontact@gmail.com>
2024-06-02 00:01:34 +0900

    [AWS]S3にディレクトリごとアップロードする

--- Assets/AWSUtils/Scripts/S3Utils.cs
+++ Assets/AWSUtils/Scripts/S3Utils.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Threading.Tasks;
 using Amazon.S3;
 using Amazon.S3.Model;
+using Amazon.S3.Transfer;
 using UnityEngine;
 
 namespace AWSUtils
@@ -16,6 +17,12 @@ namespace AWSUtils
             return new AmazonS3Client(awsAccessKeyId, awsSecretAccessKey, region);
         }
         
+        /// <summary>
+        /// フォルダの中身をアップロード
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="bucketName"></param>
+        /// <param name="folderPath"></param>
         public static async Task UploadFolder(IAmazonS3 client, string bucketName, string folderPath)
         {
             Debug.Log($"[S3Uploader]Path: {folderPath}");
@@ -36,11 +43,46 @@ namespace AWSUtils
                 }
                 catch (Exception e)
                 {
-                    Debug.LogError(e.Message);
+                    Debug.LogError($"[S3Uploader]{e.Message}");
                     throw;
                 }
             }
         }
+
+
+        /// <summary>
+        /// フォルダ以下の中身を全てアップロード（再帰）
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="bucketName"></param>
+        /// <param name="folderPath"></param>
+        public static async Task UploadAll(IAmazonS3 client, string bucketName, string folderPath)
+        {
+            Debug.Log($"[S3Uploader][All] アップロード開始. Path:{folderPath}");
+            var transferUtility = new TransferUtility(client);
+
+            TransferUtilityUploadDirectoryRequest request = new TransferUtilityUploadDirectoryRequest
+            {
+                BucketName = bucketName,
+                Directory = folderPath,
+                SearchOption = SearchOption.AllDirectories,  // 再帰的にアップロード
+                SearchPattern = "*"  // すべてのファイル
+            };
+
+            try
+            {
+                Debug.Log($"[S3Uploader][All] アップロード開始. Path:{folderPath}");
+                await transferUtility.UploadDirectoryAsync(request);
+                Debug.Log($"[S3Uploader][All] アップロード完了 Path:{folderPath}");
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"[S3Uploader]{e.Message}");
+                throw;
+            }
+
+        }
+        
         
         // ディレクトリ内のすべてのファイルパスを取得する関数
         public static string[] GetAllFiles(string directoryPath)
